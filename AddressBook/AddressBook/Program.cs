75f67f05bdbcf35078f464beaaa864edcac8c8a9
hem_b <hemanthb93@gmail.com>
2022-05-25 15:11:04 +0530

    [hemanth]:create a contacts modal class

--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -2,13 +2,11 @@
 namespace AddressBook {
     public class Program
     {
-        public List<AddressBook.ContactPerson> personDetails = new List<AddressBook.ContactPerson>();
-
+        
         public static void Main(String[] args)
         {
             AddressBook.ContactPerson person = new AddressBook.ContactPerson();
-            AddressBook.AddressBookDetails details = new AddressBook.AddressBookDetails();
-            details.createContacts();
+            
         }
     }
 }
