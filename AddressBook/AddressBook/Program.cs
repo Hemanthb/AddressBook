c40e6c126013fd4d1337bc6b062f5ff59ee931c5
hem_b <hemanthb93@gmail.com>
2022-05-25 15:15:52 +0530

    [Hemanth]:Add details of a person through console

--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -2,11 +2,15 @@
 namespace AddressBook {
     public class Program
     {
-        
+        public List<AddressBook.ContactPerson> personDetails = new List<AddressBook.ContactPerson>();
+
         public static void Main(String[] args)
         {
             AddressBook.ContactPerson person = new AddressBook.ContactPerson();
-            
+
+            AddressBook.AddressBookDetails details = new AddressBook.AddressBookDetails();
+            details.createContacts();
+
         }
     }
 }
